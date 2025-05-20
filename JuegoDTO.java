fccad672a902f7ff285e2772f335fb41e690a5f1
EmmmanuelRCPP <164968372+EmmmanuelRCPP@users.noreply.github.com>
2025-05-20 14:38:33 -0700

    xdd

---
+++ JuegoDTO.java
@@ -0,0 +1,11 @@
+public class JuegoDTO {
+        private int ganador;
+
+        public void setGanador(int ganador) {
+            this.ganador = ganador;
+        }
+        ;
+        public int getGanador() {
+            return ganador;
+        }
+    }
